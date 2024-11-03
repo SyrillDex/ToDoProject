e9a61e875b30eb36b26d0a3896541b6f77ea4814
SyrillDex <syrilldexl@gmail.com>
2024-11-03 15:14:17 +0800

    Add comments

--- js/home.js
+++ js/home.js
@@ -1,22 +1,23 @@
-let tasksList = JSON.parse(localStorage.getItem('tasksList')) || [{
-  name: "Eg. Hire an employee",
-  dueDate: "Eg. 2024-10-27",
+// create an array named taskList
+let tasksList = JSON.parse(localStorage.getItem('tasksList')) || [{ // Get the taskList from local storage
+  name: "Eg. Hire an employee",                                     // If the taskList is empty, used this
+  dueDate: "Eg. 2024-10-27",                                        // default objects using default opertator
   time: "07:00"
 }];
 
-welcomeUser();
-renderTasksList();
+
+welcomeUser(); // Render the Welcome message
+renderTasksList(); // Render the taskList
 
 function renderTasksList(){
   let tasksHTML = `
     <div class="default-tasks">
-      No tasks to show
+      No tasks to show  <!--Add this div if the taskList is empty-->
     </div>
   `;
-  let isEmpty = true;
 
-  tasksList.forEach((taskObject, index) =>{
-    const { name, dueDate, time} = taskObject;
+  tasksList.forEach((taskObject) =>{ // Loop through taskList Array
+    const { name, dueDate, time} = taskObject; // Set the taskObject variable
     const html = `
       <div class="tasks">
       <div class="buttons">
@@ -27,18 +28,18 @@ function renderTasksList(){
           <i class="fa-solid fa-trash"></i>
         </button>
       </div>
-        <p class="js-task-name">${name}</p>
-        <p><i class="fa-regular fa-calendar-days"></i> ${dueDate} </p>
-        <p><i class="fa-regular fa-clock"></i> ${time}</p>
+        <p class="js-task-name">${name}</p>   <!--Get the object name from array-->
+        <p><i class="fa-regular fa-calendar-days"></i> ${dueDate} </p> <!--Get the object dueDate from array-->
+        <p><i class="fa-regular fa-clock"></i> ${time}</p> <!--Get the object time from array-->
       </div>
       `;
-    tasksHTML += html;
+    tasksHTML += html; // Concatinate taskHTML to html string
   });
-  document.querySelector('.js-tasks').innerHTML = tasksHTML;
+  document.querySelector('.js-tasks').innerHTML = tasksHTML; // set the innerHTML of .js-tasks to taskHTML
 
-  document.querySelectorAll('.delete-button')
-    .forEach((deleteButton, index) =>{
-      deleteButton.addEventListener('click', () =>{
+  document.querySelectorAll('.delete-button') // get all the delete-buttons of the page
+    .forEach((deleteButton, index) =>{  // Loop through every button
+      deleteButton.addEventListener('click', () =>{ // Add event Listener when button is clicked
         deleteTask(index);
       });
     });
@@ -46,86 +47,88 @@ function renderTasksList(){
   document.querySelectorAll('.edit-button')
     .forEach((editButton,index) =>{
       editButton.addEventListener('click', () =>{
-        const item = tasksList[index];
-        let inputElement = document.querySelector('.task-input');
+        const item = tasksList[index]; // accesses the tasksList array using the button’s index
+        let inputElement = document.querySelector('.task-input'); // Getting input elements
         let dateInput = document.querySelector('.js-duedate-input');
         let timeInput = document.querySelector('.js-time-input');
       
-        inputElement.value = item.name;
+        inputElement.value = item.name; // Setting input values that corresponds to the value of item clicked
         dateInput.value = item.date;
         timeInput.value = item.time;
         deleteTask(index);
       });
     });
+
   function deleteTask(index){
-    tasksList.splice(index, 1);
-    saveTasksToLocalStorage();
-    renderTasksList();
+    tasksList.splice(index, 1); // Delete an item on the array based on its index
+    saveTasksToLocalStorage(); // Save the new array to local storage
+    renderTasksList(); // Render the new task List
   }
 
-  if(tasksList.length > 0){
-    isEmpty = false;
+  if(tasksList.length > 0){ // display .default-task if the array is empty
     document.querySelector('.default-tasks').style.display = "none";
   }else{
-    isEmpty = true;
     document.querySelector('.default-tasks').style.display = "flex";
   }
 }
-document.querySelector('.add-button').addEventListener('click', () =>{
+
+document.querySelector('.add-button').addEventListener('click', () =>{ // Get the button "Add"
   addToInput();
 });
 function addToInput(){
-  const inputElement = document.querySelector('.task-input');
-  const name = inputElement.value;
+  const inputElement = document.querySelector('.task-input'); // Get the task name input
+  const name = inputElement.value; // Assign its value to a variable 
 
-  const dateInput = document.querySelector('.js-duedate-input');
-  const dueDate = dateInput.value;
+  const dateInput = document.querySelector('.js-duedate-input'); // Get the date input
+  const dueDate = dateInput.value; // Assign its value to a variable 
 
-  const timeInput = document.querySelector('.js-time-input');
-  const time = timeInput.value;
+  const timeInput = document.querySelector('.js-time-input'); // Get the time input
+  const time = timeInput.value; // Assign its value to a variable 
 
-  function pushDataToArray(date,time){
-    tasksList.push({
+  function pushDataToArray(date,time){ 
+    tasksList.push({ // Adds data to the array with parameters date and time
       name,
       dueDate: date,
       time: time
     });
     saveTasksToLocalStorage();
     renderTasksList();  
-    inputElement.value = '';
+    inputElement.value = ''; // Clears the content of the input
   }
-  if(name === ""){
+  if(name === ""){ // Check if the input task name field is empty
     alert("Please input tasks");
-  }else if(!dueDate && !time){
-    pushDataToArray("No due date", "No time input");
+  }else if(!dueDate && !time){ // Check if the date and time field is empty
+    pushDataToArray("No due date", "No time input"); // Set a value to date and time by passing the parameters
   }
   else if(!dueDate){
-    pushDataToArray("No due date", time);
+    pushDataToArray("No due date", time); // If no duedate, set default duedate and pass time value
   }else if(!time){
-    pushDataToArray(dueDate, "No time input");
+    pushDataToArray(dueDate, "No time input"); // If no time, set default time and pass dueDate value
   }
   else{
-    pushDataToArray(dueDate,time);
+    pushDataToArray(dueDate,time); // If both have value, pass the values set by the user
   }
 }
-const inputElement = document.querySelector('.task-input');
+const inputElement = document.querySelector('.task-input'); // get the input element
 inputElement.addEventListener('keydown', (event)=>{
-  if(event.key === 'Enter'){
+  if(event.key === 'Enter'){ // if user pressed Enter then add the username to local storage
     addToInput();
   }
 });
 function saveTasksToLocalStorage() {
-  localStorage.setItem('tasksList', JSON.stringify(tasksList));
+    //Stringify the taskList array and add it to localStorage
+  localStorage.setItem('tasksList', JSON.stringify(tasksList)); 
 }
 function welcomeUser(){
-  let welcomeUser = JSON.parse(localStorage.getItem('username')) || "Guest";
-  let welcomeHTML = `` ;
+   // Get the username item from storage, the default is guest if no user input
+  let welcomeUser = localStorage.getItem('username') || "Guest";
+  let welcomeHTML = `` ; 
   
   const html = `
     <a href="index.html"><i class="fa-solid fa-user-pen"></i></a>
-    <h1>Welcome to <em>${welcomeUser}'s</em> To do List</h1>
+    <h1>Welcome to <em>${welcomeUser}'s</em> To do List</h1> <!--Get the username using variable-->
   `;
-  welcomeHTML += html;
+  welcomeHTML += html; // Concatinate welcome string to html
   
   document.querySelector('.welcome-mess').innerHTML = welcomeHTML;
 }
@@ -133,8 +136,6 @@ document.querySelector('.clear-button').addEventListener('click', () =>{
   clearTasks();
 })
 function clearTasks(){
-  localStorage.removeItem('tasksList');
+  localStorage.removeItem('tasksList'); // Removes taskList item from the localStorage
   window.location.reload();
 }
-
-
--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 function addUsername(){
-  const usernameInput = document.querySelector('.username');
-  const username = usernameInput.value;
+  const usernameInput = document.querySelector('.username'); // Get the username input by classname
+  const username = usernameInput.value; // Assigning the value of usernameInput to username
 
-  localStorage.setItem('username', JSON.stringify(username));
-  usernameInput.value = "";
-  window.location.href = "home.html"
+  localStorage.setItem('username', username); // Set item username to local storage named 'username'
+  usernameInput.value = ""; // Clears the value of the input field
+  window.location.href = "home.html" // Load the page home.html
 }
 
-const usernameInput = document.querySelector('.username');
-usernameInput.addEventListener('keydown', (event)=>{
-  if(event.key === 'Enter'){
+const usernameInput = document.querySelector('.username'); // Get the username input by classname
+usernameInput.addEventListener('keydown', (event)=>{ //adds event listener to usernameinput 
+  if(event.key === 'Enter'){  //if user pressed Enter then add the username to local storage
     addUsername();
   }
 });
